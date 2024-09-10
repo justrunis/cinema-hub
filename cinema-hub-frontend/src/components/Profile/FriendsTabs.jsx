c9c3d3d173c8a8134f2bce601fee15ed1e514a37
Justinas Runevičius <justinas.runevicius@ktu.edu>
2024-09-10 13:59:39 +0300

    notification  for friend requests

--- cinema-hub-frontend/src/components/Profile/FriendsTabs.jsx
+++ cinema-hub-frontend/src/components/Profile/FriendsTabs.jsx
@@ -2,8 +2,24 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import SuggestedFriends from "./SuggestedFriends";
 import FriendsList from "./FriendsList";
 import FriendRequests from "./FriendRequests";
+import { fetchUsersFriendsRequests } from "../../api/http";
+import { STALE_TIME } from "../../utils/constants";
+import { useQuery } from "@tanstack/react-query";
+import LoadingIndicator from "../UI/LoadingIndicator";
+import ErrorIndicator from "../UI/ErrorIndicator";
+import { useSelector } from "react-redux";
 
 export default function FriendsTabs() {
+  const token = useSelector((state) => state.login.token);
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["friendRequests"],
+    queryFn: () => fetchUsersFriendsRequests({ token }),
+    staleTime: STALE_TIME,
+  });
+
+  const requestsCount = data?.length;
+
   return (
     <Tabs>
       <TabList className="flex justify-around w-full border-b-2 border-base-300 mb-4">
@@ -23,7 +39,10 @@ export default function FriendsTabs() {
           className="p-2 cursor-pointer text-primary hover:text-accent"
           selectedClassName="font-bold !text-accent"
         >
-          Friend requests
+          Friend requests{" "}
+          {requestsCount > 0 && (
+            <span className="badge badge-primary ms-1">{requestsCount}</span>
+          )}
         </Tab>
       </TabList>
       <TabPanel>
